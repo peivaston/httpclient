2ba7c02b99d71af483027298750fc31c03a10469
peivaston <peivaston@hotmail.com>
2019-10-08 12:19:58 +0200

    Add initial test and Junit 5 support

---
+++ src/test/java/no/kristiania/httpclient/HttpClientTest.java
@@ -0,0 +1,13 @@
+package no.kristiania.httpclient;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HttpClientTest {
+    @Test
+    void mathSholdWork() {
+        assertEquals(4, 2+2);
+
+    }
+}
